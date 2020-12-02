f2e48d0690f02975fd6ed6ef6886da192d6c1898
Arthur Pinheiro <arthurpm@id.uff.br>
2020-12-02 16:29:54 -0300

    Adicionando geters e seters para cachorro

--- Trabalho Final/src/Cachorro.java
+++ Trabalho Final/src/Cachorro.java
@@ -23,4 +23,32 @@ public class Cachorro extends Animais{
         this.cor = cor;
         this.idade = idade;
     }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setRaça(String raça){
+        this.raça = raça;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getAnimal(){
+        return this.animal;
+    }
+    public String getRaça(){
+        return this.raça;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
 }
