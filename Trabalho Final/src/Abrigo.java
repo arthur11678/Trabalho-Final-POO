fc0466cf030bc5a61c7c7d1df74f71f2ac46b56b
Arthur Pinheiro <arthurpm@id.uff.br>
2020-12-02 21:33:42 -0300

    Alterando métodos e atributos de Abrigo

--- Trabalho Final/src/Abrigo.java
+++ Trabalho Final/src/Abrigo.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
 public class Abrigo {
-    ArrayList<Animais> animais = new ArrayList<Animais>();
-    String nomeAbrigo;
-    String nomeDono;
-    ArrayList<Adotante> adotantes = new ArrayList<Adotante>();
+    private ArrayList<Animais> animais = new ArrayList<Animais>();
+    private String nomeAbrigo;
+    private String nomeDono;
+    private ArrayList<Adotante> adotantes = new ArrayList<Adotante>();
+    private ArrayList<String> log = new ArrayList<String>();
 
     public Abrigo(String nomeAbrigo, String nomeDono){
         this.nomeAbrigo = nomeAbrigo;
         this.nomeDono = nomeDono;
     }
     public void registrarAdotante(Adotante adotante){
+        this.log.add(adotante.getNome() + " se registrou como adotante");
         this.adotantes.add(adotante);
     }
     public void registrarAnimal(Animais animal){
+        this.log.add(animal.getNome() + " foi registrado no abrigo");
         this.animais.add(animal);
     }
 
@@ -20,11 +23,32 @@ public class Abrigo {
         return this.adotantes;
     }
     
-    public ArrayList<Animais> listarAnimais(){
+    public ArrayList<Animais> getAnimais(){
         return this.animais;
     }
+    public void listarAnimais(){
+        for(Animais animal : this.animais){
+            System.out.println(animal.getNome() + " --- " + animal.getAnimal());
+        }
+    }
     public void adotarAnimal(Adotante adotante, Animais animal){
+        this.log.add(animal.getNome() + " foi adotado por :" + adotante.getNome());
         this.animais.remove(animal);
         adotante.adotar(animal);
     }
-}
+    public void getLog(){
+        System.out.println(this.log);
+    }
+    public void setNomeAbrigo(String nomeAbrigo){
+        this.nomeAbrigo = nomeAbrigo;
+    }
+    public void setNomeDono(String nomeDono){
+        this.nomeDono = nomeDono;
+    }
+    public String getNomeAbrigo(){
+        return this.nomeAbrigo;
+    }
+    public String getNomeDono(){
+        return this.nomeDono;
+    }
+}   
