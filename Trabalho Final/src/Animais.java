0c001607b48995ce731967d13018704dd119d0f0
Arthur Pinheiro <arthurpm@id.uff.br>
2020-12-02 21:34:14 -0300

    Adicionando metodos a Animais

--- Trabalho Final/src/Animais.java
+++ Trabalho Final/src/Animais.java
@@ -1,7 +1,38 @@
 abstract class Animais {
-    String nome;
-    String animal;
-    String raça;
-    String cor;
-    int idade;
+    private String nome;
+    private String animal;
+    private String raça;
+    private String cor;
+    private int idade;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setAnimal(String animal){
+        this.animal = animal;
+    }
+    public void setRaça(String raça){
+        this.raça = raça;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getAnimal(){
+        return this.animal;
+    }
+    public String getRaça(){
+        return this.raça;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
 }
